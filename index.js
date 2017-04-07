71516f15ad0ecaf050d06974225122459c239444
Erik Weisser <erikweisser@gmail.com>
2017-04-07 12:39:45 +0200

    test

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 var token = "EAAC6ygZBGWZBUBAH1Msr2r49NdZBjYCOlxay8hWgKcUo5ZAsZAOB5FZCxlA3r912Cr17ZB7ZAlAYy32oLoPUs79OaXBq9xDO6TGF5zmZBQoPieY5jMA7d1ONEx71sB8J3dOrf1j3ebjROOMfq6YocCthNiuZBJe2NOCPrYy2ACSoP7SQZDZD";
+
+var message = require("./message.js");
 var parsedWords = require('./words.json');
+
 
 
 app.set('port', (process.env.PORT || 5000))
@@ -40,15 +43,15 @@ app.post('/webhook/', function (req, res) {
                 continue;
             } else if(text.toLowerCase().includes("suche")) {
 				text = convertTextToSearchQuery(text);
-                sendTextMessage(sender, "Wie wärs wenn de selber suchst? Kannst alternativ auch hier drauf klicken: https://www.baur.de/s/" + encodeURI(text));
-                sendButton(sender, "web_type", "http://www.google.de", "Wie wärs mit Google?");
+                message.sendText(sender, "Wie wärs wenn de selber suchst? Kannst alternativ auch hier drauf klicken: https://www.baur.de/s/" + encodeURI(text));
+                message.sendButton(sender, "web_type", "http://www.google.de", "Wie wärs mit Google?");
 				continue;
 			}
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+            message.sendText(sender, "Text received, echo: " + text.substring(0, 200));
         }
         if (event.postback) {
             text = JSON.stringify(event.postback);
-            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+            message.sendText(sender, "Postback received: "+text.substring(0, 200), token);
             continue;
         }
     }
@@ -85,98 +88,13 @@ function convertTextToSearchQuery(text) {
 }
 
 function includesSearchIdentifier(text) {
-        for (var j = 0; j < parsedWords.searchIdentifier.length; j++) {
-            if(text.includes(parsedWords.searchIdentifier[j])) {
-                return true;
-            }
+    for (var j = 0; j < parsedWords.searchIdentifier.length; j++) {
+        if(text.includes(parsedWords.searchIdentifier[j])) {
+            return true;
         }
+    }
     return false;
 }
-
-// function to echo back messages
-function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
-    }
-    sendObj(sender, messageData);
-}
-
-//function to send button
-function sendButton(sender, type, url, title) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "buttons":[{
-                    "type":type,
-                    "url":url,
-                    "title":title
-                  }
-                ]
-            }
-        }
-    }
-    sendObj(sender, messageData);
-}
-
-// function to echo back messages
-function sendGenericMessage(sender) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    sendObj(sender, messageData);
-}
-
-function sendObj(sender, messageData) {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    })
-}
-
-
-
-
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
---
+++ message.js
@@ -0,0 +1,81 @@
+module.exports = {
+    sendText: function(sender, text) {
+        messageData = {
+            text:text
+        }
+        sendObj(sender, messageData);
+    },
+
+    //function to send button
+    sendButton: function(sender, type, url, title) {
+        messageData = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "buttons":[{
+                        "type":type,
+                        "url":url,
+                        "title":title
+                      }
+                    ]
+                }
+            }
+        }
+        sendObj(sender, messageData);
+    },
+
+    // function to echo back messages
+    sendGeneric: function(sender) {
+        messageData = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "First card",
+                        "subtitle": "Element #1 of an hscroll",
+                        "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": "https://www.messenger.com",
+                            "title": "web url"
+                        }, {
+                            "type": "postback",
+                            "title": "Postback",
+                            "payload": "Payload for first element in a generic bubble",
+                        }],
+                    }, {
+                        "title": "Second card",
+                        "subtitle": "Element #2 of an hscroll",
+                        "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Postback",
+                            "payload": "Payload for second element in a generic bubble",
+                        }],
+                    }]
+                }
+            }
+        }
+        sendObj(sender, messageData);
+    }
+}
+
+function sendObj(sender, messageData) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    })
+}
