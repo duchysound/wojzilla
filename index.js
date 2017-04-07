85669f6095c418779edfcfe2827f81225a1660d2
Erik Weisser <erikweisser@gmail.com>
2017-04-07 16:00:16 +0200

    test

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/webhook/', function (req, res) {
              //   message.sendJson(sender, getCommandFile(text));
                // continue;
             //}
-            message.sendText(sender, "includesCommand: " + commandJson);
+            message.sendText(sender, "includesCommand: " + __dirname);
             message.sendText(sender, "Text received, echo: " + text.substring(0, 200));
         }
         if (event.postback) {
